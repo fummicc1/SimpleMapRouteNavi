9c521144103e70c2ac4e8ac7dee3c33cb0694fdb
fummicc1 <fumiya.tennis1@gmail.com>
2019-12-21 21:23:46 +0900

    Initial Commit

---
+++ SimpleMapRouteNavi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleMapRouteNavi
+//
+//  Created by FumiyaTanaka on 2019/12/21.
+//  Copyright © 2019 FumiyaTanaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SimpleMapRouteNaviTests/SimpleMapRouteNaviTests.swift
@@ -0,0 +1,34 @@
+//
+//  SimpleMapRouteNaviTests.swift
+//  SimpleMapRouteNaviTests
+//
+//  Created by FumiyaTanaka on 2019/12/21.
+//  Copyright © 2019 FumiyaTanaka. All rights reserved.
+//
+
+import XCTest
+@testable import SimpleMapRouteNavi
+
+class SimpleMapRouteNaviTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
